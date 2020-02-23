a5b184c6b21296a5e066cc8ecc381d8732dc72e3
zhubo <zhubo@microstrategy.com>
2020-02-23 13:12:33 +0000

    global variable initializer

--- tasks/helloworld.py
+++ tasks/helloworld.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 
 hello = tf.constant('Hello, TF!')
+mylist = tf.Variable([3.142,3.201,4.019],tf.float16)
+mylist_rank = tf.rank(mylist)
+myzeros = tf.zeros([5,5,4])
 sess = tf.Session()
-
-print(sess.run(hello))
+sess.run(tf.global_variables_initializer())
+print(sess.run(myzeros))
+print(sess.run(mylist))
 sess.close()
