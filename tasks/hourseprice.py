7537f8137d6ffd09016f920a969612e8587a1677
zhubo <zhubo@microstrategy.com>
2020-02-26 02:30:36 +0000

    save

--- tasks/hourseprice.py
+++ tasks/hourseprice.py
@@ -1,6 +1,11 @@
-#https://www.cnblogs.com/mfryf/p/7904044.html
 import pandas as pd
 import seaborn as sns
+
+def normalize_feature(df):
+    return df.apply(lambda column:(column-column.mean()) / column.std())
+
 sns.set(context="notebook",style="whitegrid",palette="dark")
-df0 = pd.read_csv("data.csv",names=["square","price"])
-print(df0.head())
+df1 = pd.read_csv("data1.csv",names=["square","price"])
+df = normalize_feature(df1)
+print(df.head())
+# sns.lmplot("square","price",df0,height=6,fit_reg=True)
